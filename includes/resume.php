446dd61bd5ac09cce39cfcaf697ff84d8d781a0e
Fouad Fawzi <fouadfawzi90@gmail.com>
2017-10-20 11:20:28 +0200

    resume page has been updated

--- includes/resume.php
+++ includes/resume.php
@@ -60,31 +60,45 @@
                 <h2 class="title" data-aos="fade-up">Experience</h2>
             </div>
             <div class="details col-md-8  col-xs-12">
-                <p class="Experience-head" data-aos="fade-up">Accounting System</p>
+
+                <p data-aos="fade-up" class="Experience-head"> Worked for On-Link</p>
 
                 <p data-aos="fade-up" class="description">
-                    <B>May 2012 - Aug 2012 Freelance</B> <br/>
+                    <B>Apr. 2017 - July 2017</B><br/>
+                    Friend’s startup company (Voluntary work) <a target="_blank" href="http://onlink4it.com">ON LINK</a>
+                </p>
+
+                <!-- Item  -->
+                <p data-aos="fade-up" class="Experience-head1"> Worked for Egyptian Armed forces as IT Officer</p>
+
+                <p data-aos="fade-up" class="description">
+                    <B>Oct. 2014 - Apr. 2017</B><br/>
+                    Recruited as Reverse Officer
+                </p>
+                <!-- Item  -->
+                <p data-aos="fade-up" class="Experience-head1"> Android APP sub-team leader in Graduation project</p>
+
+                <p data-aos="fade-up" class="description">
+                    <B>Aug. 2013 - June 2014</B><br/>
+                    "At Robot and securing elderly project for <a target="_blank" href="https://www.facebook.com/ssme.Organization/?fref=ts">SSME Organization</a>"
+                </p>
+                <!-- Item  -->
+                <p data-aos="fade-up" class="Experience-head1"> Mobile remote control and monitoring</p>
+
+                <p data-aos="fade-up" class="description">
+                    <B>Aug. 2013 - June 2014</B><br/>
+                    Android application and part of my graduation project that performs full remotely
+                    control and monitor the robot from anywhere in the world via internet
+                    by the camera and control unit that fixed at the robot.
+                </p>
+                <!-- Item  -->
+                <p class="Experience-head1" data-aos="fade-up">Accounting System Freelance for an supermarket</p>
+                <p data-aos="fade-up" class="description">
+                    <B>June. 2012 – Sept. 2012 </B> <br/>
                     "Supermarket Accounting system,
                     it's a desktop application build by Java SE and Oracle SQL database
                     – 4 years experience in Java SE,
                     and 3 year experience in Oracle SQL ".
-                </p>
-
-                <p data-aos="fade-up" class="Experience-head1"> Mobile remote control and monitoring</p>
-
-                <p data-aos="fade-up" class="description">
-                    <B>SSME Organization 2013 - 2014</B><br/>
-                    "Android application is a part of the project "Robot and securing elderly"
-                    that can control and
-                    monitor the robot from anywhere
-                    in the world via internet by the camera and control unit that fixed at the robot"
-                </p>
-
-                <p data-aos="fade-up" class="Experience-head1"> Team leader</p>
-
-                <p data-aos="fade-up" class="description">
-                    <B>SSME Organization 2013 - 2014</B><br/>
-                    "At Robot and securing elderly project"
                 </p>
 
             </div>
